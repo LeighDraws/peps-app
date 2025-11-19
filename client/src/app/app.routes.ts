77136cefa3ba61fd70dedd1393c4407ddfc6c017
Leigh <psychotropes661@gmail.com>
2025-11-19 22:43:07 +0100

    ✨ CLIENT (feat) Created App Layout

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { RecipeListComponent } from 'src/features/recipe/recipes-list/recipe-list-component';
 import { HomePage } from 'src/pages/home.page/home-page';
 
 export const routes: Routes = [
   // {path: '', redirectTo: 'recipes', pathMatch: 'full'},
   { path: '', title: "Pep's | Home", component: HomePage },
+  { path: 'recipes', title: "Pep's | Recipes", component: RecipeListComponent },
 ];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
-// import { inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-// import { HealthService } from './Services/HealthService/health-service';
+import { AppLayout } from './layout/app-layout/app-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AppLayout],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  // public apiState = signal("Tester la connexion à l'API");
-  // private healthService: HealthService = inject(HealthService);
-  // callConnexion() {
-  //   this.healthService.tryConnexion();
-  //   this.apiState = this.healthService.apiState;
-  // }
-}
+export class App {}
