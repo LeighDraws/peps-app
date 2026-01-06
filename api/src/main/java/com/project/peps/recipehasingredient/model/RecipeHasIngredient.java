e05ed656131bd37ced00f637f1c6e277e2a84a22
Leigh <psychotropes661@gmail.com>
2026-01-06 19:31:31 +0100

    ✨ BACK (feat) RecipeHasIngredient Model + enum

---
+++ api/src/main/java/com/project/peps/recipehasingredient/model/RecipeHasIngredient.java
@@ -0,0 +1,65 @@
+package com.project.peps.recipehasingredient.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.project.peps.ingredient.model.Ingredient;
+import com.project.peps.recipe.model.Recipe;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = {"recipe", "ingredient"})
+@Table(name = "recipe_has_ingredient")
+public class RecipeHasIngredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ingredient_id", nullable = false)
+    private Ingredient ingredient;
+
+    @Column(nullable = false)
+    private Double quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "measurement_unit", nullable = false)
+    private MeasurementUnit measurementUnit;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
