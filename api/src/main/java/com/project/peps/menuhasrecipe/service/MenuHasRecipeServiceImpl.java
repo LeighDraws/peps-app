027500c7d58a4c2e2a1b8ff3d2c2fbc7f5309e5b
Leigh <psychotropes661@gmail.com>
2026-01-06 14:41:57 +0100

    ✨ BACK (feat) Implemented ManytoMany Relation Recipe/Menu

---
+++ api/src/main/java/com/project/peps/menuhasrecipe/service/MenuHasRecipeServiceImpl.java
@@ -0,0 +1,58 @@
+package com.project.peps.menuhasrecipe.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.project.peps.menu.model.Menu;
+import com.project.peps.menu.service.MenuService;
+import com.project.peps.menuhasrecipe.model.MenuHasRecipe;
+import com.project.peps.menuhasrecipe.repository.MenuHasRecipeRepository;
+import com.project.peps.recipe.model.Recipe;
+import com.project.peps.recipe.service.RecipeService;
+import com.project.peps.shared.exception.ResourceNotFoundException;
+
+@Service
+public class MenuHasRecipeServiceImpl implements MenuHasRecipeService {
+
+    private final MenuHasRecipeRepository menuHasRecipeRepository;
+    private final MenuService menuService;
+    private final RecipeService recipeService;
+
+    public MenuHasRecipeServiceImpl(MenuHasRecipeRepository menuHasRecipeRepository, MenuService menuService, RecipeService recipeService) {
+        this.menuHasRecipeRepository = menuHasRecipeRepository;
+        this.menuService = menuService;
+        this.recipeService = recipeService;
+    }
+
+    @Override
+    public MenuHasRecipe addRecipeToMenu(Long menuId, Long recipeId) {
+        // Verification si la recette est déjà dans le menu
+        if (menuHasRecipeRepository.existsByMenuIdAndRecipeId(menuId, recipeId)) {
+            throw new IllegalArgumentException("Recipe is already in the menu");
+        }
+
+        Menu menu = menuService.findMenuById(menuId);
+        Recipe recipe = recipeService.findRecipeById(recipeId);
+
+        MenuHasRecipe menuHasRecipe = MenuHasRecipe.builder()
+                .menu(menu)
+                .recipe(recipe)
+                .build();
+
+        return menuHasRecipeRepository.save(menuHasRecipe);
+    }
+
+    @Override
+    public void removeRecipeFromMenu(Long menuHasRecipeId) {
+        if (!menuHasRecipeRepository.existsById(menuHasRecipeId)) {
+             throw new ResourceNotFoundException("MenuHasRecipe", "id", menuHasRecipeId);
+        }
+        menuHasRecipeRepository.deleteById(menuHasRecipeId);
+    }
+
+    @Override
+    public List<MenuHasRecipe> findRecipesByMenuId(Long menuId) {
+        return menuHasRecipeRepository.findByMenuId(menuId);
+    }
+}
