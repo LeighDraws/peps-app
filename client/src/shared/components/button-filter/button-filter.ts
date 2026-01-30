9b472acf27e95ac6e4caf4f86be318fe86bcb83c
Leigh <psychotropes661@gmail.com>
2026-01-30 15:55:27 +0100

    ✨ CLIENT (feat) Created new button for filters only to implement the filtering feature

---
+++ client/src/shared/components/button-filter/button-filter.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-filter',
+  imports: [],
+  templateUrl: './button-filter.html',
+  styleUrl: './button-filter.css',
+})
+export class ButtonFilter {}
