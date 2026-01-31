d4b12e0d957af6eecb5de1d68aa4bd5c6ce2f7c8
Leigh <psychotropes661@gmail.com>
2026-01-31 13:41:20 +0100

    ♻️ BACK (refactor) Update country, ingredient, menu mappers to use mapstruct

--- api/src/main/java/com/project/peps/country/mapper/CountryMapper.java
+++ api/src/main/java/com/project/peps/country/mapper/CountryMapper.java
@@ -3,43 +3,15 @@ package com.project.peps.country.mapper;
 import com.project.peps.country.dto.CountryRequest;
 import com.project.peps.country.dto.CountryResponse;
 import com.project.peps.country.model.Country;
-import org.springframework.stereotype.Component;
-
-import java.util.Collections;
+import org.mapstruct.Mapper;
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Component
-public class CountryMapper {
+@Mapper(componentModel = "spring")
+public interface CountryMapper {
 
-    public CountryResponse toCountryResponse(Country country) {
-        if (country == null) {
-            return null;
-        }
-        return CountryResponse.builder()
-                .id(country.getId())
-                .name(country.getName())
-                .imageUrl(country.getImageUrl())
-                .build();
-    }
+    CountryResponse toCountryResponse(Country country);
 
-    public List<CountryResponse> toCountryResponseList(List<Country> countries) {
-        if (countries == null) {
-            return Collections.emptyList();
-        }
-        return countries.stream()
-                .map(this::toCountryResponse)
-                .collect(Collectors.toList());
-    }
+    List<CountryResponse> toCountryResponseList(List<Country> countries);
 
-    public Country toCountry(CountryRequest countryRequest) {
-        if (countryRequest == null) {
-            return null;
-        }
-        return Country.builder()
-                .name(countryRequest.getName())
-                .imageUrl(countryRequest.getImageUrl())
-                .build();
-    }
+    Country toCountry(CountryRequest countryRequest);
 }
-
