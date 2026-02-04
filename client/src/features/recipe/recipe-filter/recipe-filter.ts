a0cbd6bf3dc89560e5a289da43844290cb805108
Leigh <psychotropes661@gmail.com>
2026-02-04 11:14:00 +0100

    💄 CLIENT (style) Add icon and button to filters

--- client/src/features/recipe/recipe-filter/recipe-filter.ts
+++ client/src/features/recipe/recipe-filter/recipe-filter.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { ButtonFilterComponent } from 'src/shared/components/button-filter/button-filter';
+import { faCarrot, faEarthAmerica, faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-recipe-filter',
-  imports: [],
+  standalone: true,
+  imports: [ButtonFilterComponent, FontAwesomeModule],
   templateUrl: './recipe-filter.html',
   styleUrl: './recipe-filter.css',
 })
-export class RecipeFilter {}
+export class RecipeFilter {
+  carrotIcon: IconDefinition = faCarrot;
+  earthIcon: IconDefinition = faEarthAmerica;
+  utensilsIcon: IconDefinition = faUtensils;
+}
