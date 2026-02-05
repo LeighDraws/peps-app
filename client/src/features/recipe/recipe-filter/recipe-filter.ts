0155ff6f9b2f346d5b9594b069eea6d1f3b3b9d8
Leigh <145121575+LeighDraws@users.noreply.github.com>
2026-02-05 12:03:27 +0100

    ✨#S6 - Feature Recipe Filter (Country and Category) (#70)
    * 💄 CLIENT (styles) Add icons to filter buttons
    
    * 💄 CLIENT (style) Add icon and button to filters
    
    * ✨ CLIENT (feat) Added filters to recipe list
    
    * ♻️ CLIENT (refactor) Modify Category in recipe model
    
    * ✨ CLIENT (feat) implement recipe filters open modal
    
    * 🚸 CLIENT (refactor) Closing modal
    
    * 🔇 CLIENT (refactor) Remove console logs
    
    * 🐛 CLIENT (fix) Fixing issue : now category key send to backend
    
    * 🗃️ BACK (data) Updated enum for Category (impact on Recipe Schema)
    
    * ⚡️CLIENT (perf) Improved reactivity component
    
    * 🎨 CLIENT (format) format
    
    * 📝 CLIENT (docs) Added sequence diagram for recipe filter
    
    * 📝 CLIENT (docs) Update Readme
    
    * ✅ CLIENT (tests) Add e2e tests for feature filter recipes
    
    * ✅ CLIENT (tests) Add e2e tests for feature filter recipes
    
    * ✅ CLIENT (tests) update tests
    
    * ✅ CLIENT (tests) Update tests with mock
    
    * ✅ CLIENT (tests) Update tests with mock
    
    ---------
    
    Co-authored-by: Leigh <psychotropes661@gmail.com>

--- client/src/features/recipe/recipe-filter/recipe-filter.ts
+++ client/src/features/recipe/recipe-filter/recipe-filter.ts
@@ -1,9 +1,122 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  inject,
+  ViewChild,
+} from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faCarrot, faEarthAmerica, faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { take } from 'rxjs';
+import { Country } from 'src/entities/Country/model/country';
+import { CountryService } from 'src/entities/Country/service/country.service';
+import { Category, RecipeFilters } from 'src/entities/Recipe/model/recipe-filters';
+import { RecipeService } from 'src/entities/Recipe/service/recipe-service';
+import { ButtonFilterComponent } from 'src/shared/components/button-filter/button-filter';
+import { ModalComponent } from 'src/shared/components/modal/modal';
 
 @Component({
   selector: 'app-recipe-filter',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ButtonFilterComponent,
+    FontAwesomeModule,
+    ModalComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './recipe-filter.html',
   styleUrl: './recipe-filter.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RecipeFilter {}
+export class RecipeFilter {
+  @ViewChild(ModalComponent) modal!: ModalComponent;
+
+  // Injections des services et du FormBuilder
+  private fb = inject(FormBuilder);
+  private countryService = inject(CountryService);
+  private recipeService = inject(RecipeService);
+  private cd = inject(ChangeDetectorRef);
+
+  // Icones
+  carrotIcon: IconDefinition = faCarrot;
+  earthIcon: IconDefinition = faEarthAmerica;
+  utensilsIcon: IconDefinition = faUtensils;
+
+  modalTitle = '';
+  currentFilterType: 'COUNTRY' | 'CATEGORY' | 'TASTE' | null = null;
+  // Formulaire réactif pour les filtres
+  filterForm: FormGroup = this.fb.group({
+    items: this.fb.array([]),
+  });
+
+  get items(): FormArray {
+    return this.filterForm.get('items') as FormArray;
+  }
+
+  openModal(filterType: 'COUNTRY' | 'CATEGORY' | 'TASTE') {
+    this.currentFilterType = filterType;
+    this.items.clear();
+
+    if (filterType === 'COUNTRY') {
+      this.modalTitle = "Choix du pays d'origine";
+      this.countryService
+        .getAllCountries()
+        .pipe(take(1))
+        .subscribe((countries) => {
+          this.populateFormArray(countries);
+          this.cd.detectChanges();
+          this.modal.open();
+        });
+    } else if (filterType === 'CATEGORY') {
+      this.modalTitle = 'Choix de régime alimentaire';
+      const categories = Object.keys(Category).map((key) => ({
+        key,
+        label: Category[key as keyof typeof Category],
+      }));
+      this.populateFormArray(categories);
+      this.cd.detectChanges();
+      this.modal.open();
+    } else if (filterType === 'TASTE') {
+      // Implémentation pour les goûts à venir
+    }
+  }
+
+  private populateFormArray(items: (Country | { key: string; label: string })[]) {
+    items.forEach((item) => {
+      this.items.push(
+        this.fb.group({
+          value: item,
+          selected: false,
+        }),
+      );
+    });
+  }
+
+  applyFilters() {
+    const selectedItem = this.filterForm.value.items.find(
+      (item: { selected: boolean }) => item.selected,
+    );
+
+    if (selectedItem) {
+      let filters: RecipeFilters = {};
+      if (this.currentFilterType === 'COUNTRY') {
+        filters = { countryId: (selectedItem.value as Country).id };
+      } else if (this.currentFilterType === 'CATEGORY') {
+        filters = { category: selectedItem.value.key as Category };
+      }
+      this.recipeService.loadRecipes(filters);
+    } else {
+      // Optional: Load all recipes if no filter is selected
+      this.recipeService.loadRecipes();
+    }
+    this.close();
+  }
+
+  close() {
+    this.modal.onClose();
+  }
+}
--- client/src/features/recipe/recipes-list/recipe-list-component.ts
+++ client/src/features/recipe/recipes-list/recipe-list-component.ts
@@ -1,32 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { catchError, map, Observable } from 'rxjs';
+import { Component, inject, OnInit, Signal } from '@angular/core';
 import { Recipe } from 'src/entities/Recipe/model/recipe';
 import { RecipeService } from 'src/entities/Recipe/service/recipe-service';
 import { RecipeCard } from '../recipe-card/recipe-card';
+import { RecipeFilter } from '../recipe-filter/recipe-filter';
 
 @Component({
   selector: 'app-recipe-list-component',
-  imports: [CommonModule, RecipeCard],
+  standalone: true,
+  imports: [CommonModule, RecipeCard, RecipeFilter],
   templateUrl: './recipe-list-component.html',
   styleUrl: './recipe-list-component.css',
 })
 export class RecipeListComponent implements OnInit {
-  // Observable Recipes pour stocker les recettes
-  recipes$!: Observable<Recipe[]>;
+  // Signal Recipes pour stocker les recettes
+  recipes!: Signal<Recipe[]>;
 
   // Injection du service RecipeService
   private recipeService: RecipeService = inject(RecipeService);
 
   // A l'initialisation du composant, on récupère toutes les recettes depuis le service
   ngOnInit(): void {
-    this.recipes$ = this.recipeService.getAllRecipes().pipe(
-      map((recipes) => recipes || []),
-      catchError((err) => {
-        console.error('Error fetching recipes', err);
-        throw err;
-      }),
-    );
-    console.log(this.recipes$.subscribe((data) => console.log(data)));
+    this.recipes = this.recipeService.recipes;
+    this.recipeService.loadRecipes();
   }
 }
