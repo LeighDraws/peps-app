1d37d19ec5428b5cd4d78c7b00eb19be3edba04d
Leigh <psychotropes661@gmail.com>
2026-02-20 17:07:20 +0100

    ✨ CLIENT (feat) implemented register feature for frontend

--- client/src/features/auth/register/register.ts
+++ client/src/features/auth/register/register.ts
@@ -1,9 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from 'src/entities/User/service/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  pseudo = signal('');
+  email = signal('');
+  password = signal('');
+
+  errorMessage = signal<string | null>(null);
+  isLoading = signal(false);
+
+  onSubmit() {
+    if (!this.pseudo() || !this.email() || !this.password()) {
+      this.errorMessage.set('Veuillez remplir tous les champs.');
+      return;
+    }
+
+    this.isLoading.set(true);
+    this.errorMessage.set(null);
+
+    const userData = {
+      pseudo: this.pseudo(),
+      email: this.email(),
+      password: this.password(),
+    };
+
+    this.authService.register(userData).subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+      error: (error) => {
+        this.isLoading.set(false);
+        console.error('Registration failed', error);
+
+        if (error.status === 400) {
+          this.errorMessage.set('Le format des données est incorrect.');
+        } else if (error.status === 409) {
+          this.errorMessage.set('Cet email est déjà utilisé.');
+        } else {
+          this.errorMessage.set(
+            "Une erreur est survenue lors de l'inscription. Veuillez réessayer plus tard.",
+          );
+        }
+      },
+    });
+  }
+
+  onInputChange() {
+    if (this.errorMessage()) {
+      this.errorMessage.set(null);
+    }
+  }
+}
--- client/src/pages/auth/register-page/register-page.ts
+++ client/src/pages/auth/register-page/register-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Register } from 'src/features/auth/register/register';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [Register],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css',
 })
