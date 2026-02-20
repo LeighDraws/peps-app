663dfd0443434754a5ce5cd575d3afeed383d5b8
Leigh <psychotropes661@gmail.com>
2026-02-20 15:18:13 +0100

    ♻️ CLIENT (refactor) Updated routing and navbar

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { RecipeListComponent } from 'src/features/recipe/recipes-list/recipe-list-component';
+import { LoginPage } from 'src/pages/auth/login-page/login-page';
+import { RegisterPage } from 'src/pages/auth/register-page/register-page';
 import { HomePage } from 'src/pages/home.page/home-page';
 
 export const routes: Routes = [
@@ -8,6 +10,7 @@ export const routes: Routes = [
   { path: 'recipes', title: "Pep's | Recipes", component: RecipeListComponent },
   { path: 'menus', title: "Pep's | Menus", component: HomePage },
   { path: 'profile', title: "Pep's | Profile", component: HomePage },
-  { path: 'login', title: "Pep's | Login", component: HomePage },
+  { path: 'login', title: "Pep's | Login", component: LoginPage },
   { path: 'logout', title: "Pep's | Logout", component: HomePage },
+  { path: 'register', title: "Pep's | Register", component: RegisterPage },
 ];
--- client/src/shared/components/sidenav/sidenav.ts
+++ client/src/shared/components/sidenav/sidenav.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
   faHome,
   faBook,
   faCalendarDays,
-  faGears,
-  faArrowRightFromBracket,
+  faUser,
+  faPowerOff,
+  faArrowRightToBracket,
 } from '@fortawesome/free-solid-svg-icons';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from 'src/entities/User/service/auth.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -15,10 +17,17 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.css',
 })
-export class Sidenav {
+export class Sidenav implements OnInit {
   faHome = faHome;
   faBook = faBook;
   faCalendarDays = faCalendarDays;
-  faGears = faGears;
-  faArrowRightFromBracket = faArrowRightFromBracket;
+  faUser = faUser;
+  faPowerOff = faPowerOff;
+  faArrowRightToBracket = faArrowRightToBracket;
+
+  protected authService = inject(AuthService);
+
+  ngOnInit(): void {
+    console.log(this.authService.isLoaded());
+  }
 }
