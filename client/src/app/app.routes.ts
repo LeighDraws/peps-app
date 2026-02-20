e38d02a09777c6491107c3b5fdf647b57971840e
Leigh <psychotropes661@gmail.com>
2026-02-20 23:11:57 +0100

    🛂 CLIENT (refactor) Added Auth Guards to routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,14 +3,19 @@ import { RecipeListComponent } from 'src/features/recipe/recipes-list/recipe-lis
 import { LoginPage } from 'src/pages/auth/login-page/login-page';
 import { RegisterPage } from 'src/pages/auth/register-page/register-page';
 import { HomePage } from 'src/pages/home.page/home-page';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', title: "Pep's | Home", component: HomePage },
-  { path: 'recipes', title: "Pep's | Recipes", component: RecipeListComponent },
-  { path: 'menus', title: "Pep's | Menus", component: HomePage },
-  { path: 'profile', title: "Pep's | Profile", component: HomePage },
-  { path: 'login', title: "Pep's | Login", component: LoginPage },
-  { path: 'logout', title: "Pep's | Logout", component: HomePage },
-  { path: 'register', title: "Pep's | Register", component: RegisterPage },
+  { path: 'home', title: "Pep's | Accueil", component: HomePage },
+  {
+    path: 'recipes',
+    title: "Pep's | Mes Recettes",
+    component: RecipeListComponent,
+    canActivate: [authGuard],
+  },
+  { path: 'menus', title: "Pep's | Mes Menus", component: HomePage, canActivate: [authGuard] },
+  { path: 'profile', title: "Pep's | Mon Profil", component: HomePage, canActivate: [authGuard] },
+  { path: 'login', title: "Pep's | Se Connecter", component: LoginPage },
+  { path: 'register', title: "Pep's | S'inscrire", component: RegisterPage },
 ];
